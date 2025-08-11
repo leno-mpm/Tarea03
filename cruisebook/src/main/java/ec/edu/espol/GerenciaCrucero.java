1d81ee58a2406adcbeca2c54d1d85bdebe1a5a66
Danilo <danilogb18.2014@gmail.com>
2025-08-10 20:40:42 -0500

    Tests de Crucero, GerenciaCrucero y ReservaBase

--- cruisebook/src/main/java/ec/edu/espol/GerenciaCrucero.java
+++ cruisebook/src/main/java/ec/edu/espol/GerenciaCrucero.java
@@ -2,6 +2,7 @@ package ec.edu.espol;
 
 public class GerenciaCrucero {
     public void resolverProblema(String descripcion, Usuario usuario) {
+        if (descripcion == null || usuario == null) throw new IllegalArgumentException("Los parámetros no pueden ser nulos");
         System.out.println("Gerencia del crucero atiende problema: " + descripcion);
         System.out.println("Gerencia proporciona solución definitiva para usuario: " + usuario.getNombre());
         
